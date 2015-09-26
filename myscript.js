607a3f8be725adfbd5812e2470df2b9585a850e8
Tommy Barker <barker.tommy@gmail.com>
2015-09-26 17:03:21 -0500

    image now appears next to voting icons and functions TODO clean up myspcript and background

--- myscript.js
+++ myscript.js
@@ -1,8 +1,10 @@
 var btn = document.createElement("input");
-var btnUrl = chrome.extension.getURL("images/ill.jpg");
+var btnUrl = chrome.extension.getURL("images/genius.png");
 console.log(btnUrl);
 btn.src =btnUrl;
 btn.type = "image";
+btn.style.width = "80px";
+btn.style.height = "20px";
 btn.onclick = function() {
     console.log("bout to send this message");  
     chrome.runtime.sendMessage({"action": "start"}, function(response) {
