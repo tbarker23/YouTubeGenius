e6513d0459143083814657af13035df61c2ada6e
Tommy Barker <barker.tommy@gmail.com>
2015-09-27 18:07:34 -0500

    todo get some styling for the genius button

--- js/background.js
+++ js/background.js
@@ -1,206 +1,206 @@
-/* API keys and such */
-var youtubeApiUrl = "https://www.googleapis.com/youtube/v3/videos?id=";
-var geniusApiUrl = "http://api.genius.com/search?q=";
-var ytAccessKey = "key=AIzaSyDSnJWsRh_7hetLxutrfffzDT6V71iX_4w";
-var gAccessKey = "access_token=fW9FvH_s8IXkdHb_FusAmKV4jvSkyQJBvAKOXCrCuijTSoTEx8MxKlLWTqaj3opU";
-var youtubeErrorMsg = "Uh-Oh! Looks like there is some problem with YouTube API request<br><br>Are you sure you're on a YouTube.com page?";
-var geniusSongNotFound = "Uh-Oh! Looks like Genius.com doesn't have that track!";
-var referenceTitle= "";
-var geniusWinId = -1;
-var populatePref = "";
+(function() {
+    /* API keys and such */
+    var youtubeApiUrl = "https://www.googleapis.com/youtube/v3/videos?id=";
+    var geniusApiUrl = "http://api.genius.com/search?q=";
+    var ytAccessKey = "key=AIzaSyDSnJWsRh_7hetLxutrfffzDT6V71iX_4w";
+    var gAccessKey = "access_token=fW9FvH_s8IXkdHb_FusAmKV4jvSkyQJBvAKOXCrCuijTSoTEx8MxKlLWTqaj3opU";
+    /* Global Error Messages */
+    var youtubeErrorMsg = "Uh-Oh! Looks like there is some problem with YouTube API request<br><br>Are you sure you're on a YouTube.com page?";
+    var geniusSongNotFound = "Uh-Oh! Looks like Genius.com doesn't have that track!";
+    
+    var referenceTitle= ""; //title derived from youtube snippet title after consolidation
+    var geniusWinId = -1; //TODO populate same window that is created initially
+    var populatePref = "newTab"; //default setting newTab ['newTab' || 'newWin']
 
-var PopupController = function() {
-    getUrl(function(url){
-        getYoutubeInfo(url, function(youtubeInfo) {
-            getGeniusInfo(youtubeInfo, function(geniusInfo) {
-                openTab(geniusInfo);
-            });           
+    /*Getter and Setter for preference for popup.js reference */
+    function setPref(str) {
+        populatePref = str;
+    };
+    function getPref() {
+        return populatePref;
+    };
+    
+    /**
+     * PopupController - main controller of the extension.  invoked from button click in content.js
+     * @author tbarker
+     */
+    var PopupController = function() {
+        getUrl(function(url){
+            getYoutubeInfo(url, function(youtubeInfo) {
+                getGeniusInfo(youtubeInfo, function(geniusInfo) {
+                    openTab(geniusInfo);
+                });           
+            });
         });
-    });
-};
+    };
 
 
-/**
- * getUrl takes in a callback function and returns the current active tab's url
- * @param {object} callback function that will handle the url returned
- * @author tbarker
- */
-function getUrl(callback) {
-    var url = chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, 
-            function(tabs) {
-                var tab = tabs[0];
-                var url = tab.url;
-                callback(url);
-    });
-};
+    /**
+     * getUrl takes in a callback function and returns the current active tab's url
+     * @param {object} callback function that will handle the url returned
+     * @author tbarker
+     */
+    function getUrl(callback) {
+        var url = chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, 
+                function(tabs) {
+                    var tab = tabs[0];
+                    var url = tab.url;
+                    callback(url);
+        });
+    };
 
-/**
- * consolidateQuery - will take in the title of a youtube content video and strip out
- * all of the useless junk including [ 'ft.', 'featuring, '[*]', '(*)', ' x ', and ','
- * @param {string} vTitle title string from YouTube
- * @author tbarker
- */
-function consolidateQuery(vTitle) {
-    if(vTitle.indexOf("-") > -1) {
-        var str = vTitle.split('-');
-        var artistInfo = str[0];
-        var trackInfo = str[1];
-
-        console.log(str);
-        var artistInfo = artistInfo.replace(/ *\([^)]*\) */g, ""); //removes parens and content
-        var artistInfo = artistInfo.replace(/ *\[.*\]* */g, ""); //removes squares and contents
-        var artistInfo = artistInfo.replace(/ *\sx\s* */g, " "); //removes all ' x ' features
-        var artistInfo = artistInfo.replace(/ *,.*\- /g, "-"); //replaces featurings with comms
-        var artistInfo = artistInfo.replace(/ *ft.* */g, ""); //removes all ft. chars
-        var artistInfo = artistInfo.replace(/ *featuring.* */g, ""); //removes all featuring's
-        var artistInfo = artistInfo.replace(/ *HD* */g, ""); 
-        
-        var trackInfo = trackInfo.replace(/ *\([^)]*\) */g, ""); //removes parens and contents
-        var trackInfo = trackInfo.replace(/ *\[.*\]* */g, ""); //removes squares and contents
-        var trackInfo = trackInfo.replace(/ *\sx\s* */g, " "); //removes all ' x ' features
-        var trackInfo = trackInfo.replace(/ *,.*\- /g, "-"); //replaces featurings with comms
-        var trackInfo = trackInfo.replace(/ *ft.* */g, ""); //removes all ft. chars
-        var trackInfo = trackInfo.replace(/ *featuring.* */g, ""); //removes all featuring's
-        var trackInfo = trackInfo.replace(/HD/g, " ");
-        console.log("consolidateQuery \t artistInfo = " + artistInfo 
-                + "\t trackInfo =" + trackInfo); 
-        return artistInfo + trackInfo;
-    } else {
-        //for weird videos without the - separating artist/title
-        var vTitle = vTitle.replace(/ *\([^)]*\) */g, ""); //removes parens and content
-        var vTitle = vTitle.replace(/ *\[.*\]* */g, ""); //removes squares and contents
-        var vTitle = vTitle.replace(/ *\sx\s* */g, " "); //removes all ' x ' features
-        var vTitle = vTitle.replace(/ *,.*\- /g, "-"); //replaces featurings with comms
-        var vTitle = vTitle.replace(/ *ft.* */g, ""); //removes all ft. chars
-        var vTitle = vTitle.replace(/ *featuring.* */g, ""); //removes all featuring's
-        var vTitle = vTitle.replace(/ *HD* */g, "");
-
-        return vTitle;
-    } 
-
-}
-
-/*coalesceGeniusResults -  will take the JSON result set returned from the genius
- * API.  This function primarily is a check for 'New Music Day' results and simply gets
- * then next list item (generally but not always correct) which seems to be the actual
- * content lyrics
- * @param {object} resultSet JSON results from the genius API call
- * @param {string} vidTitle the title of the youtube video  
- * @author tbarker
- */
-function coalesceGeniusResults(resultSet, vidTitle) {
-    var result = {};
-    var i = 0;
-    //console.log(resultSet); //results returned from genius api
-    if(resultSet.length > 0) {
-        for(i = 0; i < resultSet.length; i++) {
-            if(vidTitle.toLowerCase().indexOf(resultSet[i].result.title.toLowerCase()) 
-                    > -1){
-                return resultSet[i].result.url;
+    /**
+     * getYouTubeInfo - executes the api call to googleapis.com (youtube) after getting
+     * the video id from the url parameters passed in.
+     * @param {string} - url string from the current tab gotten from getUrl()
+     * @author tbarker
+     */
+    var getYoutubeInfo=function(url, callback) {
+        var videoId = url.split("v=")[1];
+        $.ajax({
+            url: youtubeApiUrl + videoId + '&' 
+            + ytAccessKey +'&part=snippet,contentDetails,statistics,status',
+            dataType: 'json', 
+            success: function(data) {
+                callback(data);
+            },
+            error: function(data) {
+                alert(youtubeErrorMsg);
             }
-        }    
-        return resultSet[0].result.url;
-    } else {
-        alert(geniusSongNotFound);
-    }
-};
-
-/**
- * OpenTab - takes the data from genius.com and makes sure there is a result
- * then will execute a new tab/new window with the genius url.
- * @param {object} data JSON obj recieved from api.genius.com
- */
-function openTab(data) {
-    if(data.response.hits.size == 0) {
-        alert(geniusSongNotFound);
-    } else {
-        var geniusUrl = coalesceGeniusResults(data.response.hits, referenceTitle);
-        if(populatePref == "newWin") {
-            if(geniusUrl != null) {
-                chrome.windows.getCurrent(function(win) {
-                    chrome.windows.update(win.id, {width: screen.width*.5});
-                });
-                
-                chrome.windows.create({
-                    url: geniusUrl, 
-                    left: screen.width, 
-                    width: screen.width*.5, 
-                    height: screen.height }, function(data){
-                        geniusWinId = data.id;
-                });
-            } 
-        } else {
-            if(geniusUrl != null) {
-                chrome.tabs.create({url: geniusUrl});
-            }
-        }
-    }
-};
-
-
-/**
- * getGeniusInfo - executes the api call to api.genius.com with the title information
- * pulled from api.google.com (youtube)
- * @param {object} data JSON obj recieved from youtube api api.google.com
- * @author - tbarker
- */
-function getGeniusInfo(data, callback) {
-    //console.log(data); //info from youtube api
-    if(data.items.length == 0)
-        alert(youtubeErrorMsg);
-    var vidTitle = data.items[0].snippet.title;
-    referenceTitle = consolidateQuery(vidTitle);
-    $.ajax({
-        url: geniusApiUrl + referenceTitle + "&" + gAccessKey,
-        dataType: 'json',
-        success: function(data) {
-            callback(data);
-        },
-        error: function(data) {
-            alert(geniusSongNotFound);
-            //document.getElementById('statusMsg').innerHTML = geniusSongNotFound;
-        }
-    });        
-        
-};
-
-/**
- * getYouTubeInfo - executes the api call to googleapis.com (youtube) after getting
- * the video id from the url parameters passed in.
- * @param {string} - url string from the current tab gotten from getUrl()
- * @author tbarker
- */
-var getYoutubeInfo=function(url, callback) {
-    var videoId = url.split("v=")[1];
-    $.ajax({
-        url: youtubeApiUrl + videoId + '&' 
-        + ytAccessKey +'&part=snippet,contentDetails,statistics,status',
-        dataType: 'json', 
-        success: function(data) {
-            callback(data);
-        },
-        error: function(data) {
+        });
+    };
+    
+    /**
+     * getGeniusInfo - executes the api call to api.genius.com with the title information
+     * pulled from api.google.com (youtube)
+     * @param {object} data JSON obj recieved from youtube api api.google.com
+     * @author - tbarker
+     */
+    function getGeniusInfo(data, callback) {
+        //console.log(data); //info from youtube api
+        if(data.items.length == 0)
             alert(youtubeErrorMsg);
+        var vidTitle = data.items[0].snippet.title;
+        referenceTitle = consolidateQuery(vidTitle);
+        $.ajax({
+            url: geniusApiUrl + referenceTitle + "&" + gAccessKey,
+            dataType: 'json',
+            success: function(data) {
+                callback(data);
+            },
+            error: function(data) {
+                alert(geniusSongNotFound);
+                //document.getElementById('statusMsg').innerHTML = geniusSongNotFound;
+            }
+        });        
+            
+    };
+
+    /**
+     * OpenTab - takes the data from genius.com and makes sure there is a result
+     * then will execute a new tab/new window with the genius url.
+     * @param {object} data JSON obj recieved from api.genius.com
+     */
+    function openTab(data) {
+        if(data.response.hits.size == 0) {
+            alert(geniusSongNotFound);
+        } else {
+            var geniusUrl = coalesceGeniusResults(data.response.hits, referenceTitle);
+            if(populatePref == "newWin") {
+                if(geniusUrl != null) {
+                    chrome.windows.getCurrent(function(win) {
+                        chrome.windows.update(win.id, {width: screen.width*.5});
+                    });
+                    
+                    chrome.windows.create({
+                        url: geniusUrl, 
+                        left: screen.width, 
+                        width: screen.width*.5, 
+                        height: screen.height }, function(data){
+                            geniusWinId = data.id;
+                    });
+                } 
+            } else {
+                if(geniusUrl != null) {
+                    chrome.tabs.create({url: geniusUrl});
+                }
+            }
         }
+    };
+
+    /**
+     * consolidateQuery - will take in the title of a youtube content video and strip out
+     * all of the useless junk including [ 'ft.', 'featuring, '[*]', '(*)', ' x ', and ','
+     * @param {string} vTitle title string from YouTube
+     * @author tbarker
+     */
+    function consolidateQuery(vTitle) {
+        if(vTitle.indexOf("-") > -1) {
+            var str = vTitle.split('-');
+            var artistInfo = str[0];
+            var trackInfo = str[1];
+
+            console.log(str);
+            var artistInfo = artistInfo.replace(/ *\([^)]*\) */g, ""); //removes parens and content
+            var artistInfo = artistInfo.replace(/ *\[.*\]* */g, ""); //removes squares and contents
+            var artistInfo = artistInfo.replace(/ *\sx\s* */g, " "); //removes all ' x ' features
+            var artistInfo = artistInfo.replace(/ *,.*\- /g, "-"); //replaces featurings with comms
+            var artistInfo = artistInfo.replace(/ *ft.* */g, ""); //removes all ft. chars
+            var artistInfo = artistInfo.replace(/ *featuring.* */g, ""); //removes all featuring's
+            var artistInfo = artistInfo.replace(/ *HD* */g, ""); 
+            
+            var trackInfo = trackInfo.replace(/ *\([^)]*\) */g, ""); //removes parens and contents
+            var trackInfo = trackInfo.replace(/ *\[.*\]* */g, ""); //removes squares and contents
+            var trackInfo = trackInfo.replace(/ *\sx\s* */g, " "); //removes all ' x ' features
+            var trackInfo = trackInfo.replace(/ *,.*\- /g, "-"); //replaces featurings with comms
+            var trackInfo = trackInfo.replace(/ *ft.* */g, ""); //removes all ft. chars
+            var trackInfo = trackInfo.replace(/ *featuring.* */g, ""); //removes all featuring's
+            var trackInfo = trackInfo.replace(/HD/g, " ");
+            console.log("consolidateQuery \t artistInfo = " + artistInfo 
+                    + "\t trackInfo =" + trackInfo); 
+            return artistInfo + trackInfo;
+        } else {
+            //for weird videos without the - separating artist/title
+            var vTitle = vTitle.replace(/ *\([^)]*\) */g, ""); //removes parens and content
+            var vTitle = vTitle.replace(/ *\[.*\]* */g, ""); //removes squares and contents
+            var vTitle = vTitle.replace(/ *\sx\s* */g, " "); //removes all ' x ' features
+            var vTitle = vTitle.replace(/ *,.*\- /g, "-"); //replaces featurings with comms
+            var vTitle = vTitle.replace(/ *ft.* */g, ""); //removes all ft. chars
+            var vTitle = vTitle.replace(/ *featuring.* */g, ""); //removes all featuring's
+            var vTitle = vTitle.replace(/ *HD* */g, "");
+
+            return vTitle;
+        } 
+
+    }
+
+    /*coalesceGeniusResults -  will take the JSON result set returned from the genius
+     * API.  This function primarily is a check for 'New Music Day' results and simply gets
+     * then next list item (generally but not always correct) which seems to be the actual
+     * content lyrics
+     * @param {object} resultSet JSON results from the genius API call
+     * @param {string} vidTitle the title of the youtube video  
+     * @author tbarker
+     */
+    function coalesceGeniusResults(resultSet, vidTitle) {
+        var result = {};
+        var i = 0;
+        //console.log(resultSet); //results returned from genius api
+        if(resultSet.length > 0) {
+            for(i = 0; i < resultSet.length; i++) {
+                if(vidTitle.toLowerCase().indexOf(resultSet[i].result.title.toLowerCase()) 
+                        > -1){
+                    return resultSet[i].result.url;
+                }
+            }    
+            return resultSet[0].result.url;
+        } else {
+            alert(geniusSongNotFound);
+        }
+    };
+
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        console.log("recieved event from geniusBtn");
+        window.PC = new PopupController();
     });
-};
-
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log("recieved event from geniusBtn");
-    window.PC = new PopupController();
-});
-
-/** 
- * Sets the populating preference (by tab or by window 
- */
-function setPref(str) {
-    populatePref = str;
-};
-
-/**
- * Gets the populating preference (by tab or by window)
- */
-function getPref() {
-    return populatePref;
-};
+})();
