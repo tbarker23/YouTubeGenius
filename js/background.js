7285d90bfb6a675d35f4f0c10ae526ed05d3142b
Tommy BARKER <Tommy BARKER>
2016-09-10 13:28:55 -0500

    cleaned up indentation and brought content js up to coding standards. cleaned up indentation in css and background js files

--- js/background.js
+++ js/background.js
@@ -14,8 +14,8 @@ var populatePref = ""; //default setting newTab ['newTab' || 'newWin']
 
 /*Getter and Setter for preference for popup.js reference */
 function setPref(str) {
-    console.log(str);
-    populatePref = str;
+  //console.log(str);
+  populatePref = str;
 }
 function getPref() {
     return populatePref;
@@ -26,13 +26,13 @@ function getPref() {
  * @author tbarker
  */
 var PopupController = function() {
-    getUrl(function(url){
-        getYoutubeInfo(url, function(youtubeInfo) {
-            getGeniusInfo(youtubeInfo, function(geniusInfo) {
-                openTab(geniusInfo);
-            });           
-        });
+  getUrl(function(url){
+    getYoutubeInfo(url, function(youtubeInfo) {
+      getGeniusInfo(youtubeInfo, function(geniusInfo) {
+        openTab(geniusInfo);
+      });           
     });
+  });
 };
 
 
@@ -42,12 +42,13 @@ var PopupController = function() {
  * @author tbarker
  */
 function getUrl(callback) {
-    var url = chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, 
-            function(tabs) {
-                var tab = tabs[0];
-                var url = tab.url;
-                callback(url);
-    });
+  var url = chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, 
+        function(tabs) {
+            var tab = tabs[0];
+            var url = tab.url;
+            callback(url);
+        }
+  );
 }
 
 /**
@@ -57,18 +58,18 @@ function getUrl(callback) {
  * @author tbarker
  */
 var getYoutubeInfo=function(url, callback) {
-    var videoId = url.split("v=")[1];
-    $.ajax({
-        url: youtubeApiUrl + videoId + '&' 
-        + ytAccessKey +'&part=snippet,contentDetails,statistics,status',
-        dataType: 'json', 
-        success: function(data) {
-            callback(data);
-        },
-        error: function(data) {
-            alert(youtubeErrorMsg);
-        }
-    });
+  var videoId = url.split("v=")[1];
+  $.ajax({
+    url: youtubeApiUrl + videoId + '&' 
+    + ytAccessKey +'&part=snippet,contentDetails,statistics,status',
+    dataType: 'json', 
+    success: function(data) {
+        callback(data);
+    },
+    error: function(data) {
+        alert(youtubeErrorMsg);
+    }
+  });
 };
 
 /**
@@ -78,24 +79,23 @@ var getYoutubeInfo=function(url, callback) {
  * @author - tbarker
  */
 function getGeniusInfo(data, callback) {
-    //console.log(data); //info from youtube api
-    if(data.items.length === 0)
-        alert(youtubeErrorMsg);
-    var vidTitle = data.items[0].snippet.title;
-    referenceTitle = consolidateQuery(vidTitle);
-    console.log(referenceTitle);
-    $.ajax({
-        url: geniusApiUrl + referenceTitle + "&" + gAccessKey,
-        dataType: 'json',
-        success: function(data) {
-            callback(data);
-        },
-        error: function(data) {
-            alert(geniusSongNotFound);
-            //document.getElementById('statusMsg').innerHTML = geniusSongNotFound;
-        }
-    });        
-        
+  //console.log(data); //info from youtube api
+  if(data.items.length === 0)
+    alert(youtubeErrorMsg);
+  var vidTitle = data.items[0].snippet.title;
+  referenceTitle = consolidateQuery(vidTitle);
+  //console.log(referenceTitle);
+  $.ajax({
+    url: geniusApiUrl + referenceTitle + "&" + gAccessKey,
+    dataType: 'json',
+    success: function(data) {
+        callback(data);
+    },
+    error: function(data) {
+        alert(geniusSongNotFound);
+        //document.getElementById('statusMsg').innerHTML = geniusSongNotFound;
+    }
+  });        
 }
 
 /**
@@ -104,50 +104,51 @@ function getGeniusInfo(data, callback) {
  * @param {object} data JSON obj recieved from api.genius.com
  */
 function openTab(data) {
-    if(data.response.hits.size === 0) {
-        alert(geniusSongNotFound);
+  if(data.response.hits.size === 0) {
+    alert(geniusSongNotFound);
+  } else {
+    var geniusUrl = coalesceGeniusResults(data.response.hits, referenceTitle);
+    if(populatePref == "newWin") {
+      if(geniusUrl !== null) {
+        chrome.windows.getCurrent(function(win) {
+          chrome.windows.update(win.id, {width: screen.width * 0.5});
+        });
+        
+        chrome.windows.create({
+          url: geniusUrl, 
+          left: screen.width, 
+          width: screen.width * 0.5, 
+          height: screen.height }, function(data){
+              geniusWinId = data.id;
+        });
+      } 
+    } else if(populatePref == "inPage") {
+      if(geniusUrl !== null) {
+        $.ajax({url: geniusUrl, 
+            success: function(data) {
+              var i = 0;
+             var geniusHtml = data;
+             chrome.tabs.query({active: true, currentWindow: true}, 
+                 function(tabs) {
+                     chrome.tabs.sendMessage(tabs[0].id, 
+                         {action: "addLyrics2Page", lyrics:  geniusHtml},
+                         function(response) {
+                         });
+                  }
+              );
+            }
+        }); 
+      }
     } else {
-        var geniusUrl = coalesceGeniusResults(data.response.hits, referenceTitle);
-        if(populatePref == "newWin") {
-            if(geniusUrl !== null) {
-                chrome.windows.getCurrent(function(win) {
-                    chrome.windows.update(win.id, {width: screen.width * 0.5});
-                });
-                
-                chrome.windows.create({
-                    url: geniusUrl, 
-                    left: screen.width, 
-                    width: screen.width * 0.5, 
-                    height: screen.height }, function(data){
-                        geniusWinId = data.id;
-                });
-            } 
-        } else if(populatePref == "inPage") {
-            if(geniusUrl !== null) {
-               $.ajax({url: geniusUrl, 
-                       success: function(data) {
-                           var i = 0;
-                           var geniusHtml = data;
-                           chrome.tabs.query({active: true, currentWindow: true}, 
-                               function(tabs) {
-                                   chrome.tabs.sendMessage(tabs[0].id, 
-                                       {action: "addLyrics2Page", lyrics:  geniusHtml},
-                                       function(response) {
-                                       });
-                               });
-                       }
-               }); 
-            }
-        } else {
-            if(geniusUrl !== null) {
-                chrome.tabs.create({url: geniusUrl});
-            }
+        if(geniusUrl !== null) {
+          chrome.tabs.create({url: geniusUrl});
         }
     }
+  }
 }
 
 function annotationOnClick(id) {
-    alert(id);
+  alert(id);
 }
 
 /**
@@ -157,43 +158,43 @@ function annotationOnClick(id) {
  * @author tbarker
  */
 function consolidateQuery(vTitle) {
-    if(vTitle.indexOf("-") > -1) {
-        var str = vTitle.split('-');
-        var artistInfo = str[0];
-        var trackInfo = str[1];
+  if(vTitle.indexOf("-") > -1) {
+    var str = vTitle.split('-');
+    var artistInfo = str[0];
+    var trackInfo = str[1];
 
-        artistInfo = artistInfo.replace(/\([^)]*\)/g, ""); //({})'s
-        artistInfo = artistInfo.replace(/\[.*\]/g, ""); //[{}]
-        artistInfo = artistInfo.replace(/\sx\s*/g, " "); //' x ' features
-        artistInfo = artistInfo.replace(/ *,.*\- /g, "-"); //,'s
-        artistInfo = artistInfo.replace(/ft. +[A-z ]*/g, ""); //ft. {}'s
-        artistInfo = artistInfo.replace(/ *featuring.* */g, ""); //featuring's
-        artistInfo = artistInfo.replace(/HD [A-z\u00C0-\u017F, ]*/g, ""); //HD's
-        artistInfo = artistInfo.replace(/\+ [A-z\u00C0-\u017F, ]*/g, "");//+{}'s
-        
-        trackInfo = trackInfo.replace(/\([^)]*\)/g, ""); //({})'s
-        trackInfo = trackInfo.replace(/\[.*\]/g, ""); //[{}]'s
-        trackInfo = trackInfo.replace(/\sx\s*/g, " "); //' x ' features's
-        trackInfo = trackInfo.replace(/ *,.*\- /g, "-"); //,'s
-        trackInfo = trackInfo.replace(/ft. +[A-z\u00C0-\u017F, ]*/g, ""); //ft. {}'s
-        trackInfo = trackInfo.replace(/ *featuring.* */g, ""); //featuring's
-        trackInfo = trackInfo.replace(/HD [A-z\u00C0-\u017F, ]*/g, " ");//HD's
-        trackInfo = trackInfo.replace(/\+ [A-z\u00C0-\u017F, ]*/g, "");//+{}'s
-        console.log("consolidateQuery \t artistInfo = " + artistInfo 
-                + "\t trackInfo =" + trackInfo); 
-        return artistInfo + trackInfo;
+    artistInfo = artistInfo.replace(/\([^)]*\)/g, ""); //({})'s
+    artistInfo = artistInfo.replace(/\[.*\]/g, ""); //[{}]
+    artistInfo = artistInfo.replace(/\sx\s*/g, " "); //' x ' features
+    artistInfo = artistInfo.replace(/ *,.*\- /g, "-"); //,'s
+    artistInfo = artistInfo.replace(/ft. +[A-z ]*/g, ""); //ft. {}'s
+    artistInfo = artistInfo.replace(/ *featuring.* */g, ""); //featuring's
+    artistInfo = artistInfo.replace(/HD [A-z\u00C0-\u017F, ]*/g, ""); //HD's
+    artistInfo = artistInfo.replace(/\+ [A-z\u00C0-\u017F, ]*/g, "");//+{}'s
+    
+    trackInfo = trackInfo.replace(/\([^)]*\)/g, ""); //({})'s
+    trackInfo = trackInfo.replace(/\[.*\]/g, ""); //[{}]'s
+    trackInfo = trackInfo.replace(/\sx\s*/g, " "); //' x ' features's
+    trackInfo = trackInfo.replace(/ *,.*\- /g, "-"); //,'s
+    trackInfo = trackInfo.replace(/ft. +[A-z\u00C0-\u017F, ]*/g, ""); //ft. {}'s
+    trackInfo = trackInfo.replace(/ *featuring.* */g, ""); //featuring's
+    trackInfo = trackInfo.replace(/HD [A-z\u00C0-\u017F, ]*/g, " ");//HD's
+    trackInfo = trackInfo.replace(/\+ [A-z\u00C0-\u017F, ]*/g, "");//+{}'s
+    console.log("consolidateQuery \t artistInfo = " + artistInfo 
+            + "\t trackInfo =" + trackInfo); 
+    return artistInfo + trackInfo;
     } else {
-        //for weird videos without the - separating artist/title
-        vTitle = vTitle.replace(/\([^)]*\)/g, ""); //({})'s
-        vTitle = vTitle.replace(/\[.*\]/g, ""); //[{}]'s
-        vTitle = vTitle.replace(/\sx\s*/g, " "); //' x ' features
-        vTitle = vTitle.replace(/ *,.*\- /g, "-"); //,{}'s
-        vTitle = vTitle.replace(/ft. +[A-z\u00C0-\u017F,]*/g, ""); //ft. {}'
-        vTitle = vTitle.replace(/ *featuring.* */g, ""); //featuring's
-        vTitle = vTitle.replace(/HD [A-z\u00C0-\u017F, ]*/g, ""); //HD's
-        vTitle = vTitle.replace(/\+ [A-z\u00C0-\u017F, ]*/g, "");//+{}'s
+      //for weird videos without the - separating artist/title
+      vTitle = vTitle.replace(/\([^)]*\)/g, ""); //({})'s
+      vTitle = vTitle.replace(/\[.*\]/g, ""); //[{}]'s
+      vTitle = vTitle.replace(/\sx\s*/g, " "); //' x ' features
+      vTitle = vTitle.replace(/ *,.*\- /g, "-"); //,{}'s
+      vTitle = vTitle.replace(/ft. +[A-z\u00C0-\u017F,]*/g, ""); //ft. {}'
+      vTitle = vTitle.replace(/ *featuring.* */g, ""); //featuring's
+      vTitle = vTitle.replace(/HD [A-z\u00C0-\u017F, ]*/g, ""); //HD's
+      vTitle = vTitle.replace(/\+ [A-z\u00C0-\u017F, ]*/g, "");//+{}'s
 
-        return vTitle;
+      return vTitle;
     } 
 
 }
@@ -207,19 +208,19 @@ function consolidateQuery(vTitle) {
  * @author tbarker
  */
 function coalesceGeniusResults(resultSet, vidTitle) {
-    var result = {};
-    var i = 0;
-    console.log(resultSet); //results returned from genius api
-    if(resultSet.length > 0) {
-        for(i = 0; i < resultSet.length; i++) {
-            if(vidTitle.toLowerCase().indexOf(resultSet[i].result.title.toLowerCase()) 
-                    > -1){
-                return resultSet[i].result.url;
-            }
-        }    
-        return resultSet[0].result.url;
+  var result = {};
+  var i = 0;
+  //console.log(resultSet); //results returned from genius api
+  if(resultSet.length > 0) {
+    for(i = 0; i < resultSet.length; i++) {
+      if(vidTitle.toLowerCase().indexOf(resultSet[i].result.title.toLowerCase()) 
+                                              > -1){
+            return resultSet[i].result.url;
+      }
+    }    
+    return resultSet[0].result.url;
     } else {
-        alert(geniusSongNotFound);
+      alert(geniusSongNotFound);
     }
 }
 
@@ -231,50 +232,50 @@ function coalesceGeniusResults(resultSet, vidTitle) {
  * @author tbarker
  */
 function getAnnotation(id) {
-    $.ajax({
-        url: geniusAnnotationUrl + id + "?" + gAccessKey,
-        dataType: 'json',
-        success: function(data) {
-            console.log(data);
-            var annotations = data.response.annotation.body.dom.children;
-            var html = "";
-            console.log(annotations);
-            for(var i=0; i < annotations.length; i++) {
-                if(annotations[i].tag == "p") {
-                    html += "<" + annotations[i].tag + ">";
-                    for(var j=0; j < annotations[i].children.length; j++) {
-                        if(typeof annotations[i].children[j] != "object") 
-                        html += annotations[i].children[j];
-                    }
-                    if(Array.isArray(annotations[i].children)) {
-                      for(var k=0; k < annotations[i].children.length; k++) {
-                        if(annotations[i].children[k].tag == "img") {
-                          html += "<img style=\"width: 200px;\" src=\"" + annotations[i].children[k].attributes.src + "\" />";
-                        }
-                      }
-                    }
-                    html += "<" + annotations[i].tag + "/>";
-                }
+  $.ajax({
+    url: geniusAnnotationUrl + id + "?" + gAccessKey,
+    dataType: 'json',
+    success: function(data) {
+      //console.log(data);
+      var annotations = data.response.annotation.body.dom.children;
+      var html = "";
+      console.log(annotations);
+      for(var i=0; i < annotations.length; i++) {
+        if(annotations[i].tag == "p") {
+          html += "<" + annotations[i].tag + ">";
+          for(var j=0; j < annotations[i].children.length; j++) {
+            if(typeof annotations[i].children[j] != "object") 
+              html += annotations[i].children[j];
             }
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.sendMessage(tabs[0].id,
-                                        {action: "populateModal", annotation: html},
-                                        function(response) {}
-                                        );
-            });
-        },
-        error: function(data) {
-            alert(geniusSongNotFound);
+            if(Array.isArray(annotations[i].children)) {
+              for(var k=0; k < annotations[i].children.length; k++) {
+                if(annotations[i].children[k].tag == "img") {
+                  html += "<img style=\"width: 200px;\" src=\"" + annotations[i].children[k].attributes.src + "\" />";
+                }
+              }
+            }
+            html += "<" + annotations[i].tag + "/>";
         }
-    });
+      }
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                        chrome.tabs.sendMessage(tabs[0].id,
+                                  {action: "populateModal", annotation: html},
+                                  function(response) {}
+                                  );
+      });
+    },
+    error: function(data) {
+      alert(geniusSongNotFound);
+    }
+  });
 }    
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log("recieved event from geniusBtn: " + request.action);
-    if(request.action == "getAnnotation") {
-        console.log(request.id);
-        getAnnotation(request.id);
-    } else {
-        window.PC = new PopupController();
+  console.log("recieved event from geniusBtn: " + request.action);
+  if(request.action == "getAnnotation") {
+    //console.log(request.id);
+    getAnnotation(request.id);
+  } else {
+    window.PC = new PopupController();
     }
 });
--- js/content.js
+++ js/content.js
@@ -15,84 +15,83 @@ var linkIds = [];
  * @author tbarker
  */
 function createBtn() {   
-    var btn = document.createElement("input");
-    btn.type = "image";
-    var btnUrl = chrome.extension.getURL("images/genius.png");
-    btn.src = btnUrl;
-    btn.className = "like-button-renderer actionable";
-    
-    //style it
-    btn.style.width = "80px";
-    btn.style.height = "20px";
-    
-    //give the btn some functionality
-    btn.onclick = function() {
-        chrome.runtime.sendMessage({"action": "start"}, function(response) {
-        });
-    };
-    return btn;
+  var btn = document.createElement("input");
+  btn.type = "image";
+  var btnUrl = chrome.extension.getURL("images/genius.png");
+  btn.src = btnUrl;
+  btn.className = "like-button-renderer actionable";
+  
+  //style it
+  btn.style.width = "80px";
+  btn.style.height = "20px";
+  
+  //give the btn some functionality
+  btn.onclick = function() {
+    chrome.runtime.sendMessage({"action": "start"}, function(response) {
+    });
+  };
+  return btn;
 }
 
 var geniusBtn = createBtn();
 
-//add button to the page whenever DOM changes (ajax workaround from document.ready)
+//add button to page whenever DOM changes (ajax workaround from document.ready)
 document.addEventListener("DOMSubtreeModified", function() {
-        document.getElementById("watch8-sentiment-actions").appendChild(geniusBtn);
+  document.getElementById("watch8-sentiment-actions").appendChild(geniusBtn);
 }, false);
 
 /**
- * consolidateHtml - takes in a string of html gotten from curl on genius lyric page
- * and splices out only the lyrics section defined by content between .lyrics_container
- * and .song_footer
+ * consolidateHtml - takes in a string of html gotten from curl on genius lyric 
+ * page and splices out only the lyrics section defined by content between
+ * .lyrics_container and .song_footer
  * @param str string of html
  * @return relevantHtml spliced html of lyrics content
  * @author tbarker
  */
 function consolidateHtml(str) {
-   var relevantHtml = str.substring(str.indexOf("<lyrics *>")+1);
-   relevantHtml = relevantHtml.substring(0,relevantHtml.indexOf("</lyrics>"));
-    return relevantHtml;
-
+  var relevantHtml = str.substring(str.indexOf("<lyrics *>")+1);
+  relevantHtml = relevantHtml.substring(0,relevantHtml.indexOf("</lyrics>"));
+  return relevantHtml;
 }
 
 /**
  * annotationOnClick - onclick handler for all the links in the lyrics returned 
- * will send a message to the background page which will handle the api call to genius
- * based on the id of the annotation.
+ * will send a message to the background page which will handle the api call to 
+ * genius based on the id of the annotation.
  * @author tbarker
  */
 function annotationOnClick(id) {
-    console.log("about to send getAnnotation: " + id);
-   chrome.runtime.sendMessage({action: "getAnnotation", id: id}, 
-           function(response) {});
+  console.log("about to send getAnnotation: " + id);
+  chrome.runtime.sendMessage({action: "getAnnotation", id: id}, 
+                              function(response) {});
 }
 
 /**
- * addOnClicks - will take in an html string and access the <a> links in the <p> section
- * will remove the href attribute and assign an ID based on the data-id.  
+ * addOnClicks - will take in an html string and access the <a> links in the <p>
+ * section will remove the href attribute and assign an ID based on the data-id.  
  * @param relevantHtml - html string of lyrics
- * @return links - DOM object of the relevanthtml with hrefs removed and ID added
+ * @return links - DOM obj of the relevanthtml with hrefs removed and ID added
  * @author tbarker
  */
 function addOnClicks(relevantHtml) {
-   var relHTML = relevantHtml.replace("<p>", "");
-   relHTML = relHTML.replace("</p>", "");
-   var html = $.parseHTML(relHTML);
+  var relHTML = relevantHtml.replace("<p>", "");
+  relHTML = relHTML.replace("</p>", "");
+   
+  var html = $.parseHTML(relHTML);
    //console.log(relevantHtml);
-   var links = $(".lyrics  ", html);
-   console.log(links);
+  var links = $(".lyrics  ", html);
    //iterate over links and add onclick and remove href to <a>
-   for(i = 0; i < links[0].children.length; i++) {
-       if(links[0].children[i].tagName == 'A') {
-       links[0].children[i].removeAttribute("href");
-       links[0].children[i].id = links[0].children[i].getAttribute("data-id");
-       linkIds.push(links[0].children[i].id);
-       links[0].children[i].onclick = function() {
-           annotationOnClick(this.id);
-       };
-     }
-   }
-   return links;
+  for(i = 0; i < links[0].children.length; i++) {
+    if(links[0].children[i].tagName == 'A') {
+      links[0].children[i].removeAttribute("href");
+      links[0].children[i].id = links[0].children[i].getAttribute("data-id");
+      linkIds.push(links[0].children[i].id);
+      links[0].children[i].onclick = function() {
+        annotationOnClick(this.id);
+      };
+    }
+  }
+  return links;
 }
 
 /**
@@ -103,13 +102,13 @@ function addOnClicks(relevantHtml) {
  * @author tbarker
  */
 function setUpListeners() {
-    var i = 0;
-    for(i = 0; i < linkIds.length; i++) {
-        var anchor = document.getElementById(linkIds[i]);
-        $('#'+linkIds[i]).click(function() {
-            annotationOnClick(this.id);
-        });
-    }
+  var i = 0;
+  for(i = 0; i < linkIds.length; i++) {
+    var anchor = document.getElementById(linkIds[i]);
+    $('#'+linkIds[i]).click(function() {
+      annotationOnClick(this.id);
+    });
+  }
 }
 
 /**
@@ -120,59 +119,60 @@ function setUpListeners() {
  * @author tbarker
  */
 function createAnnotationModal(annotationText) {
-    var overlay = (document.getElementById('overlay') !==  null ? 
-        document.getElementById('overlay') : document.createElement('div'));
-    overlay.id = "overlay";
+  var overlay = (document.getElementById('overlay') !==  null ? 
+            document.getElementById('overlay') : document.createElement('div'));
+  overlay.id = "overlay";
     
-    var innerDiv = document.createElement('div');
-    var titleText = document.createElement('span');
-    titleText.id = "titleText";
-    var closeBtn = document.createElement('span');
-    closeBtn.id = "closeBtn";
-    
-    var pTag = document.createElement('p');
-    pTag.id = "annotationText";
+  var innerDiv = document.createElement('div');
+  var titleText = document.createElement('span');
+  titleText.id = "titleText";
+  var closeBtn = document.createElement('span');
+  closeBtn.id = "closeBtn";
+  
+  var pTag = document.createElement('p');
+  pTag.id = "annotationText";
 
-    pTag.innerHTML = "";
-    pTag.innerHTML = annotationText; 
-    
-    closeBtn.innerHTML = "X";
-    titleText.innerHTML = "Genius.com Says: ";
-    closeBtn.onclick = function() {
-        var rehide = document.getElementById("overlay");
-        var youtubePage = document.getElementById("page");
-        youtubePage.style.opacity = "1";
-        rehide.style.visibility = "hidden";
-    };
-    innerDiv.appendChild(titleText);
-    innerDiv.appendChild(closeBtn);
-    innerDiv.appendChild(pTag);
-    overlay.appendChild(innerDiv);
-    document.body.appendChild(overlay);
+  pTag.innerHTML = "";
+  pTag.innerHTML = annotationText; 
+  
+  closeBtn.innerHTML = "X";
+  titleText.innerHTML = "Genius.com Says: ";
+  closeBtn.onclick = function() {
+    var rehide = document.getElementById("overlay");
+    var youtubePage = document.getElementById("page");
+    youtubePage.style.opacity = "1";
+    rehide.style.visibility = "hidden";
+  };
+  innerDiv.appendChild(titleText);
+  innerDiv.appendChild(closeBtn);
+  innerDiv.appendChild(pTag);
+  overlay.appendChild(innerDiv);
+  document.body.appendChild(overlay);
 }
 
 /**
- * addLyricsToPage - overlays the lyrics returned from the genius API on the comment
- * section of the YouTube page.  TODO if youtube layout changes watch-discussions will 
- * have to be changed to the new id of the new comment section.
+ * addLyricsToPage - overlays the lyrics returned from the genius API on the
+ * comment section of the YouTube page.  TODO if youtube layout changes 
+ * watch-discussions will have to be changed to the new id of the new comment
+ * section.
  * @param lyrics innerHTML of lyric container from genius lyric page
  * @author tbarker
  */
 function addLyricsToPage(lyrics) {
-        var str = consolidateHtml(lyrics);
-        //console.log(str);
-        var links = addOnClicks(str);
-        console.log(links);
-        var output = "";
-        links[0].id = "lyricsContainer";
-        for(i = 0; i < links.length; i++) {
-            output += links[i].outerHTML;
-        }
-        output = output;
-        document.getElementById("watch-discussion").innerHTML = "";
-        document.getElementById("watch-discussion").innerHTML = output;
-        setUpListeners();
-        linkIds = [];
+  var str = consolidateHtml(lyrics);
+  //console.log(str);
+  var links = addOnClicks(str);
+  //console.log(links);
+  var output = "";
+  links[0].id = "lyricsContainer";
+  for(i = 0; i < links.length; i++) {
+    output += links[i].outerHTML;
+  }
+  output = output;
+  document.getElementById("watch-discussion").innerHTML = "";
+  document.getElementById("watch-discussion").innerHTML = output;
+  setUpListeners();
+  linkIds = [];
 }
 
 /**
