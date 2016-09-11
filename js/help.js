f8ae1c4c0b47acb53f699b9cc460490a9bf58b52
tbarker23 <barker.tommy@gmail.com>
2016-09-10 21:15:15 -0500

    utilize popup.js now for shared radio buttons between popup and help page.

--- js/help.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-if(chrome.extension.getBackgroundPage().getPref() == "newWin")
-        newWinRadio.checked = true;
-    else if(chrome.extension.getBackgroundPage().getPref() == "inPage")
-        inPageCommentRadio.checked= true;
-    else
-        newTabRadio.checked = true;
-});
